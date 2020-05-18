7b9e0d54ae0fc83d0aab36cb2edb26ebc53aeda0
Amie Croteau <ac04400@ravens.racc.edu>
2020-05-18 17:33:25 -0400

    Completed Project

--- lib/main.dart
+++ lib/main.dart
@@ -11,61 +11,70 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/download.png'),
-            ),
-            Text('Amie Croteau',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                )),
-            Text(
-              'SOFTWARE ENGINEER ASSOCIATE',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 20.0,
-                color: Colors.teal[100],
-                letterSpacing: 2.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/download.png'),
               ),
-            ),
-            Container(
+              Text('Amie Croteau',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  )),
+              Text(
+                'SOFTWARE ENGINEER ASSOCIATE',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.phone, color: Colors.teal, size: 20.0),
-                    SizedBox(width: 10.00),
-                    Text('702-445-2831',
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: ListTile(
+                    leading: Icon(Icons.link, color: Colors.teal, size: 20.0),
+                    title: Text('https://www.linkedin.com/in/amie-croteau/',
                         style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ],
-                )),
-            Container(
+                          fontSize: 15.0,
+                        )),
+                  ),
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.mail, color: Colors.teal, size: 20.0),
-                    SizedBox(width: 10.00),
-                    Text('amiecroteau@gmail.com',
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: ListTile(
+                    leading: Icon(Icons.mail, color: Colors.teal, size: 20.0),
+                    title: Text('amiecroteau@email.com',
                         style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ],
-                ))
-          ],
-        )),
+                          fontSize: 15.0,
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
