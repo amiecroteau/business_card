6d8189e9edd67f9f3cfdd4be1804034d8d931191
Amie Croteau <ac04400@ravens.racc.edu>
2020-05-18 16:50:42 -0400

    Adjustments made to create the card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/download.png'),
+            ),
+            Text('Amie Croteau',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                )),
+            Text(
+              'SOFTWARE ENGINEER ASSOCIATE',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 20.0,
+                color: Colors.teal[100],
+                letterSpacing: 2.5,
+              ),
+            ),
+            Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal, size: 20.0),
+                    SizedBox(width: 10.00),
+                    Text('702-445-2831',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ))
+                  ],
+                )),
+            Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.mail, color: Colors.teal, size: 20.0),
+                    SizedBox(width: 10.00),
+                    Text('amiecroteau@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ))
+                  ],
+                ))
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
